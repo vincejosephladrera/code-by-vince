97836c7cbb90e02c2e058092ccea52afb58a1d06
Vince Joseph Ladrera <ladreravincejoseph@gmail.com>
2025-05-21 22:55:17 +0800

    chore: initialize Next.js app structure

--- src/api/getTodos.ts
+++
@@ -1,6 +0,0 @@
-export async function getTodos() {
-	const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-	const data = response.json();
-
-	return data;
-}
--- src/components/custom-ui-components/Container.tsx
+++
@@ -1,7 +0,0 @@
-import { cn } from '@/lib/utils';
-
-function Container({ children, className }: React.HtmlHTMLAttributes<HTMLDivElement>) {
-	return <div className={cn('container mx-auto', className)}>{children}</div>;
-}
-
-export default Container;
