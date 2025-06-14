91a1fe7c453896139c0445408159c829904f0375
Vince Joseph Ladrera <ladreravincejoseph@gmail.com>
2025-06-14 18:28:57 +0800

    chore: remove server-side config (tRPC, Prisma) and unused React components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { HydrateClient } from "@/trpc/server";
 import HomeHero from "@/features/about/components/HomeHero";
 import PowerUps from "@/features/about/components/PowerUps";
 import EpicQuests from "@/features/quests/components/EpicQuests";
@@ -6,11 +5,11 @@ import DeveloperPreview from "@/features/about/components/DeveloperPreview";
 
 export default async function Home() {
   return (
-    <HydrateClient>
+    <>
       <HomeHero />
       <PowerUps />
       <DeveloperPreview />
       <EpicQuests />
-    </HydrateClient>
+    </>
   );
 }
